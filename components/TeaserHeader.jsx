d023f05e17f8c193ce096d5d4e83b0b4595f5a58
Shums Siwani <shums.siwani@virginmediao2.co.uk>
2025-07-01 14:50:40 +0100

    another commit

--- components/TeaserHeader.jsx
+++ components/TeaserHeader.jsx
@@ -4,7 +4,7 @@ export default function Teaser({ blok }) {
 
 return (
 	<div className="teaser" data-cy="teaser" {...storyblokEditable(blok)}>
-		<h2>{blok.headline}</h2>
+		<h1>{blok.headline}</h1>
 	</div>
 );
 }
